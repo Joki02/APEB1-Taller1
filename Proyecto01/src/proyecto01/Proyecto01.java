b704c5b2ee5284bc98d7d7b224d927edb4a997cc
Joki02 <bjalao@utpl.edu.ec>
2023-10-20 16:56:32 -0500

    primer commit

---
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author Edwin Miguel
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Ejemplo básico de Java");
+    System.out.println("Versionado a través de Git");
+    System.out.println("Manejado desde GitHub");
+    System.out.println ("Recuerde usar la sentencia");
+    }
+    
+}
